685d767b98094dabd5754dfd670924f540ccdf54
arham zakki edelo <azedelo1999@gmail.com>
2022-02-14 13:31:04 +0700

    Add def try2

--- remote_repo.py
+++ remote_repo.py
@@ -1,15 +1,15 @@
 print("Hello World, zakki here, good to see you!!")
 
 class Bagi():
-  def __init__(self,a,b):
-    self.a = a
-    self.b = b
-  def hasil(self):
-    self.rslt = self.a // self.b
-    return("Hasil bagi {} dengan {} adalah {}".format(self.a, self.b, self.rslt))
-  
-  def print(self):
-    print("Semangat yoo, kau pasti bisa")
-    
-  def try1(self):
-    print("ya pastilah semangat")
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def hasil(self):
+        self.rslt = self.a // self.b
+        return("Hasil bagi {} dengan {} adalah {}".format(self.a, self.b, self.rslt))
+    def print(self):
+        print("Semangat yoo, kau pasti bisa")
+        
+    def try1(self):
+        print("ya pastilah semangat")
+
